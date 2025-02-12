8a23c01a118d238b21c6b00b698197fe44a259be
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-02-12 12:00:58 +0100

    Co-authored-by: E-Eriksson <E-Eriksson@users.noreply.github.com> Co-authored-by: Sofia <SofiaSyntax@users.noreply.github.com> Co-authored-by: Julia  Berglund <juliberglund@users.noreply.github.com> Co-authored-by: Ali Yusuf <Alichas1@users.noreply.github.com>

---
+++ src/pages/news/articles/[id].js
@@ -0,0 +1,24 @@
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users/" + id
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      user: data,
+    },
+  };
+}
+
+export default function SSRuser({ user }) {
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
+    </div>
+  );
+}
