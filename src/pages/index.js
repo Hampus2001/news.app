e427da7b8686975701b0d004c0d3eedae71be485
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-02-13 11:31:57 +0100

    Added pokemon API

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,22 @@
 
 export async function getServerSideProps() {
   //hämta data här:
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch("https://api.pokemontcg.io/v2/cards/xy1-1");
   const data = await response.json();
 
   return {
-    props: { users: data },
+    props: { pokemon: data },
   };
 }
 
 // All kod körs på servern och
 // det som returneras från komponenten skickas till browsern
-export default function Home({ users }) {
+export default function Home({ pokemon }) {
   return (
     <div>
-      {users.map((user) => (
-        <p>
-          <a href={"/news/articles/" + user.id}>{user.name}</a>
-        </p>
-      ))}
+      <p>
+        <a href={"/news/articles/" + pokemon.data.id}>{pokemon.data.name}</a>
+      </p>
     </div>
   );
 }
--- src/pages/news/articles/[id].js
+++ src/pages/news/articles/[id].js
@@ -1,24 +1,21 @@
 export async function getServerSideProps(context) {
   const id = context.params.id;
 
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/users/" + id
-  );
+  const response = await fetch("https://api.pokemontcg.io/v2/cards/" + id);
   const data = await response.json();
 
   return {
     props: {
-      user: data,
+      pokemon: data,
     },
   };
 }
 
-export default function SSRuser({ user }) {
+export default function SSRuser({ pokemon }) {
   return (
     <div>
-      <h1>{user.name}</h1>
-      <p>{user.email}</p>
-      <p>{user.phone}</p>
+      <img src={pokemon.data.images.large} />
+      <p>{pokemon.data.tcgplayer.prices.holofoil.market} dollar</p>
     </div>
   );
 }
